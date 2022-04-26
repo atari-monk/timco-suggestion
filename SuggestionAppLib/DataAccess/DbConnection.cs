16cbd6c4f63821bd4acad0e34a71e04928ecb874
krzm <kmazanek@gmail.com>
2022-04-26 12:09:17 +0200

    Setup DbConnection

--- SuggestionAppLib/DataAccess/DbConnection.cs
+++ SuggestionAppLib/DataAccess/DbConnection.cs
@@ -1,6 +1,35 @@
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace SuggestionAppLib.DataAccess;
 
 public class DbConnection
 {
-    
+    private readonly IConfiguration config;
+    private readonly IMongoDatabase db;
+    private string connectionId = "MongoDb";
+
+    public string DbName { get; private set; }
+    public string CategoryCollectionName { get; private set; }  = "categories";
+    public string StatusCollectionName { get; private set; }  = "statuses";
+    public string UserCollectionName { get; private set; }  = "users";
+    public string SuggestionsCollectionName { get; private set; }  = "suggestions";
+    public MongoClient Client { get; private set; }
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollection { get; private set; }
+    public IMongoCollection<SuggestionModel> SuggestionsCollection { get; private set; }
+
+    public DbConnection(IConfiguration config)
+    {
+        this.config = config;
+        Client = new MongoClient(
+            config.GetConnectionString(connectionId));
+        DbName = config["DatabaseName"];
+        db = Client.GetDatabase(DbName);
+        CategoryCollection = db.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = db.GetCollection<StatusModel>(StatusCollectionName);
+        UserCollection = db.GetCollection<UserModel>(UserCollectionName);
+        SuggestionsCollection = db.GetCollection<SuggestionModel>(SuggestionsCollectionName);
+    }
 }
